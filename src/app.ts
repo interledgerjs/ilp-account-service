c5f6bd96f82f5792eb3092d319a1796fe6102f13
donovan <donovanchangfoot@gmail.com>
2018-11-22 15:28:08 +0200

    add app.ts and index.ts

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { AccountInfo } from './types/accounts'
+import { PluginInstance } from './types/plugin'
+import GrpcPluginProxyAccountService, { ConnectorInfo } from './account_services/grpc-plugin-proxy'
+import PluginAccountService from './account_services/plugin'
+import { GrpcTransport } from 'ilp-transport-grpc'
+import GrpcAccountService from './account_services/grpc'
+
+export function createOutOfProcessPluginAccountService (accountId: string, accountInfo: AccountInfo, plugin: PluginInstance, connectorInfo: ConnectorInfo, disabledMiddleware?: string[]) {
+  return new GrpcPluginProxyAccountService(accountId, accountInfo, plugin, connectorInfo, disabledMiddleware)
+}
+
+export function createProxyAccountService (accountId: string, accountInfo: AccountInfo, stream: GrpcTransport) {
+  return new GrpcAccountService(accountId, accountInfo, stream)
+}
+
+export function createInprocessPluginAccountService (accountId: string, accountInfo: AccountInfo, plugin: PluginInstance, disabledMiddleware: string[]) {
+  return new PluginAccountService(accountId, accountInfo, plugin, disabledMiddleware)
+}
